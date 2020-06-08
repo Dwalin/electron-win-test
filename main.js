b3f7ff7a2883761e1f7728675e5d8fb53e15d516
Oles <ibolosig@gmail.com>
2020-06-08 21:34:40 +0300

    Initial.

--- main.js
+++ main.js
@@ -4,58 +4,80 @@ const {
   BrowserWindow,
   ipcMain,
 } = require('electron');
+// const console = require('console');
+// app.console = new console.Console(process.stdout, process.stderr);
+// process.stdout.write('your output to command prompt console or node js ');
 
-// app.commandLine.appendSwitch("disable-gpu");
-// app.commandLine.appendArgument("disable-gpu");
+console.log = (message) => {
+  if (stripNode) {
+    // stripNode.executeJavaScript(`console.log("console.log from electron")`);
+    stripNode.executeJavaScript(`console.log(${JSON.stringify(message)})`);
+  }
+};
 
 app.disableHardwareAcceleration();
-
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let stripWindow;
 let stripNode;
 
-var FFILA = require('ffi-napi');
-var REF = require('ref-napi');
+const { U } = require('win32-api');
+const ffi = require('ffi-napi');
+const ref = require('ref-napi');
 
-var user32IsWindow = new FFILA.Library('user32', {
-  'IsWindow' : [
-    'BOOL', [ 'int32' ]
-  ]
+let pfnWinEventProc = null;
+pfnWinEventProc = ffi.Callback("void", ["pointer", "int", "pointer", "long", "long", "int", "int"], () => {
+  console.log('HELLO');
 });
+
+const user32 = U.load();
+
+setInterval(() => {
+  const appWindow = user32.GetForegroundWindow();
+  console.log(appWindow);
+
+  user32.SetWindowTextW(appWindow, Buffer.from('OKOK' + '\0', 'ucs2'));
+
+  const msgType = ref.types.void;
+  const msgPtr = ref.refType(msgType);
+  const EVENT_SYSTEM_FOREGROUND = 3;
+  const WINEVENT_OUTOFCONTEXT = 0;
+  const WINEVENT_SKPIOWNPROCESS = 2;
+
+  user32.SetWinEventHook(
+    1,
+    3,
+    appWindow,
+    pfnWinEventProc,
+    0,
+    0,
+    0 | 2
+  );
+
+  // user32.SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, null, pfnWinEventProc,
+  //   0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKPIOWNPROCESS)
+
+}, 1000);
+
+
+
 
 
 const launchStrip = () => {
   // Create the browser window.
 
   stripWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    frame: false,
-    useContentSize: true,
-    // alwaysOnTop: true,
-    transparent: true,
+    width: 600,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  stripWindow.loadURL('https://app.jivaro.com');
+  stripWindow.loadURL(`file://${__dirname}/test.html`);
   stripNode = stripWindow && stripWindow.webContents;
 
-  console.log("I LIKE TURTLES");
+  // setInterval(func, 1000);
 
-  const res = user32IsWindow.IsWindow(2232174);
-  console.log(res);
-
-  // const res = REF.alloc(32);
-  // var dres = res.deref();
-  //
-  // if (dres == 0) console.log("NOT A WINDOW");
-  // if (dres == 1) console.log("IS A WINDOW");
-  // Emitted when the window is closed.
-
+  console.log("Strip launched");
 
   stripWindow.on('closed', () => {
     stripNode = null;
@@ -63,38 +85,13 @@ const launchStrip = () => {
   });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   setTimeout(() => {
-    // if (process.platform === 'win32') {
-    //   app.setAsDefaultProtocolClient('jivaro', process.execPath, [
-    //     path.resolve(process.argv[1] || ''),
-    //   ]);
-    //
-    //   // app.setAsDefaultProtocolClient('jivaro');
-    // } else {
-    //   app.setAsDefaultProtocolClient('jivaro');
-    // }
-
     launchStrip();
   }, 400);
-
 });
 
-// Quit when all windows are closed.
+
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit();
 });
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // if (stripWindow === null) launchStrip();
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
